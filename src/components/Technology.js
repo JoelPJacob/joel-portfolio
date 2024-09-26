cd4fc67babbff93afc964c6d1a055b945158b6b1
joel <joel.jacob@roadelabs.com>
2024-09-26 18:36:07 +0530

    feat-moving banner designing

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -4,15 +4,22 @@ import ProfilePicture from '../assets/images/joel-img.jpg';
 import { Git, Linkedin, GitWhite, LinkedinWhite } from '../assets/icons';
 import { useTheme } from '../context/ThemeContext';
 import Marquee from "react-fast-marquee";
+import './componentsStyles/Technology.css';
 
 
 const Technology = () => {
     return (
-        <div style={{marginLeft:'20px'}}>
-        <Marquee speed={30} delay={0} loop={0} gradientColor={'rgb(248,251,253)'} gradient={true}>
-            <img width="900px" src="https://skillicons.dev/icons?i=react,html,css,js,bootstrap,materialui,flutter,postman,figma,firebase,github,vscode,npm,netlify,bitbucket" />
-        </Marquee>
-        </div>
+        <>
+            <div>
+                <p className='main-heading'>
+                    My Tech Stacks
+                </p>
+            </div><div className='condainer'>
+                <Marquee speed={70} delay={0} gradientColor={'rgb(248,251,253)'} gradient={true} gradientWidth={10}>
+                    <img width="1500px" src="https://skillicons.dev/icons?i=react,html,css,js,bootstrap,materialui,flutter,postman,figma,firebase,github,vscode,npm,netlify,bitbucket" />
+                </Marquee>
+            </div>
+        </>
     );
 };
 
