3f4bbf9259325e3a974adf450e86b11988e633e5
joel <joel.jacob@roadelabs.com>
2024-09-28 21:43:58 +0530

    Designed Experience, Skills, and Education sections with enhanced UI elements and interactivity.

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,51 +1,94 @@
 import React from 'react';
 import './componentsStyles/Education.css';
-import { Git } from "../assets/icons";
 import { useTheme } from '../context/ThemeContext';
+import CircularProgressBar from './CircularProgressBar';
+import { ReactJs, JavaScript, Flutter, Figma, Html, Css, PostMan } from '../assets/icons'
 
 let educationElements = [
   {
     id: 1,
-    title: "Master of Computer Application",
-    location: "Union Christian College, Aluva, Kerala",
+    designition: "Senior Software Engineer",
+    organization: "RoadE Labs Pvt Ltd",
+    location: "Kakkanad, Kochi, Kerala",
     description:
-      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-    date: "2019 - 2022",
-    icon: "work",
+      "Responsible for designing user-friendly applications using React, JavaScript, and CSS, focusing on web performance optimization.",
+    date: "DEC To Present",
+    year: "2023",
   },
   {
     id: 2,
-    title: "Bachelor of Computer Application",
-    location: "CMS College, Kottayam, Kerala",
+    designition: "Junior Software Engineer",
+    organization: "RoadE Labs Pvt Ltd",
+    location: "Kakkanad, Kochi, Kerala",
     description:
-      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-    date: "2016 - 2019",
-    icon: "work",
+      "Contributed to web development projects with React and helped integrate APIs for seamless data interaction.",
+    date: "NOV To NOV 2023",
+    year: "2022",
   },
+  {
+    id: 3,
+    designition: "Intern",
+    organization: "RoadE Labs Pvt Ltd",
+    location: "Kakkanad, Kochi, Kerala",
+    description:
+      "Worked on front-end development projects, gaining experience in HTML, CSS, and JavaScript.",
+    date: "AUG To OCT 2019",
+    year: "2019",
+  },
+
+];
+let skills = [
+  { id: 1, name: "React Native", percentage: 70, icon: <ReactJs /> },
+  { id: 2, name: "React JS", percentage: 80, icon: <ReactJs /> },
+  { id: 3, name: "JavaScript", percentage: 90, icon: <JavaScript /> },
+  { id: 4, name: "HTML", percentage: 85, icon: <Html /> },
+  { id: 5, name: "CSS", percentage: 75, icon: <Css /> },
+  { id: 6, name: "Flutter", percentage: 65, icon: <Flutter /> },
+  { id: 7, name: "Postman", percentage: 70, icon: <PostMan /> },
+  { id: 8, name: "Figma", percentage: 60, icon: <Figma /> },
 ];
 
 const Education = () => {
   const { theme } = useTheme();
-  
+  const isOddCount = educationElements.length % 2 !== 0;
+
   return (
-    <div className="container">
+    <>
       <div>
-        <p className="main-heading">Education</p>
+        <p className="main-heading">Experience</p>
       </div>
-      <div className="education">
-        {educationElements.map((element) => (
-          <div key={element.id} className={`education-element ${theme === 'dark' ? 'dark' : 'light'}`}>
-            <div className="education-content">
-              <p className='sub-heading'>{element.title}</p>
-              <p className='main-heading-h1'>{element.location}</p>
-              <p className='main-heading-h1'>{element.date}</p>
-              <p className='sub-text'>{element.description}</p>
+      <div className="container">
+        <div className={`education ${isOddCount ? 'odd-count' : ''}`}>
+          {educationElements.map((element) => (
+            <div key={element.id} className={`education-element ${theme === 'dark' ? 'dark' : 'light'}`}>
+              <div className="year">{element.year}
+                <span className="date-text">{element.date}</span>
+              </div>
+              <div className="education-content">
+                <p>{element.designition}</p>
+                <p>{element.organization}</p>
+                <p>{element.location}</p>
+                <p>{element.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <p className="main-heading">Skills</p>
+        <div className="skills-container">
+          {skills.map(skill => (
+            <div key={skill.id} className="skill-item">
+              <div className="skill-icon">{skill.icon}</div>
+              <p className='skill-title'>{skill.name}</p>
+              <CircularProgressBar percentage={skill.percentage} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
+
 
 export default Education;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,58 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./componentsStyles/Experience.css";
+import { useTheme } from '../context/ThemeContext';
 
 const experienceData = [
   {
     id: 1,
-    title: "Software Engineer",
-    company: "RoadE Labs Pvt. Ltd.",
-    duration: "Nov 2022 - Present",
+    title: "Master of Computer Application",
+    company: "Union Christian College, Aluva, Kerala",
+    duration: "Sep 2019 - Aug 2022",
     year: "2024",
-    description:
-      "Managed project TaskSphere, leading mobile app development and API testing.",
   },
   {
     id: 2,
-    title: "Junior Finance Executive",
-    company: "Marymatha Infrastructure Pvt Ltd",
-    duration: "Oct 2023 - Feb 2024",
+    title: "Bachelor of Computer Application",
+    company: "CMS College, Kottayam, Kerala",
+    duration: "May 2016 - Aug 2019",
     year: "2023",
-    description:
-      "Executed financial strategies and managed client consultations to increase revenue.",
   },
   // Add more experiences here
 ];
 
-const Experience = () => {
-  return (
-    <><div>
-      <p className="main-heading">Experience</p>
-    </div>
-    <div className="timeline">
-    {experienceData.map((experience, index) => (
-  <div
-    key={experience.id}
-    className={`timeline-item ${index % 2 === 0 ? "left" : "right"}`}
-  >
-    <div className="timeline-dot" />
-    <div
-      className={`timeline-year timeline-year-${experience.year}`}
-    >
-      {experience.year}
-    </div> 
-    <div className="timeline-content-flex">
-      <div className="timeline-content">
-        <h3 className="timeline-title">{experience.title}</h3>
-        <h4 className="timeline-company">{experience.company}</h4>
-        <p className="timeline-duration">{experience.duration}</p>
-        <p className="timeline-description">{experience.description}</p>
-      </div>
-    </div>
-  </div>
-))}
+const certifications = [
+  "The Complete React Native + Hooks Course – Udemy",
+  "React - The Complete Guide 2024 (incl. React Router & Redux) – Udemy",
+  "React Native: Advanced Concepts – Udemy",
+  "React Native Expo Chat Application with Firebase – Udemy",
+  "From Developer to Team Lead - Effective Dev Team Leading – Udemy",
+  "Master React Native Animations – Udemy"
+];
 
+const Experience = () => {
+  const { theme } = useTheme();
+  const [currentCertificationIndex, setCurrentCertificationIndex] = useState(0);
+
+  // Function to change certification
+  const changeCertification = () => {
+    setCurrentCertificationIndex((prevIndex) => (prevIndex + 1) % certifications.length);
+  };
+
+  // Automatically change certification every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(changeCertification, 5000);
+    return () => clearInterval(interval); // Clean up on unmount
+  }, []);
+
+  return (
+    <>
+      <div>
+        <p className="main-heading">Education</p>
       </div>
-      </>
+      <div className="timeline">
+        {experienceData.map((experience, index) => (
+          <div
+            key={experience.id}
+            className={`timeline-item ${index % 2 === 0 ? "left" : "right"}`}
+          >
+            <div className="timeline-dot" />
+            <div className={`timeline-year timeline-year-${experience.year}`}>
+              {experience.year}
+            </div>
+            <div className="timeline-content-flex">
+              <div className="timeline-content">
+                <h3 className="sub-heading">{experience.title}</h3>
+                <p className="sub-text">{experience.company}</p>
+                <p className="timeline-duration">{experience.duration}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p className="main-heading">Certifications</p>
+      </div>
+      <div className="certification-container"> {/* New wrapper for center alignment */}
+        <div className="certification-border">
+          <p className="certification">{certifications[currentCertificationIndex]}</p>
+          <div className="circle-container">
+            <div 
+              className={`circle ${currentCertificationIndex === 0 ? 'active' : ''}`}
+              onClick={() => setCurrentCertificationIndex(0)}
+            />
+            <div 
+              className={`circle ${currentCertificationIndex === 1 ? 'active' : ''}`}
+              onClick={() => setCurrentCertificationIndex(1)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
